99308a41cc3a0b3927afca0b9f525ff2872ed99e
Pedro Faria <pedrofaria.mc@gmail.com>
2022-01-31 11:57:41 -0300

    refactoring commands support

--- cypress/integration/LoginPage.js
+++ cypress/integration/LoginPage.js
@@ -1,56 +1,52 @@
-describe("Tests Login Page", () => {
-    beforeEach(() => cy.visit("https://qa.postclickmarketing.com/Admin/Login"));
+/// <reference types="Cypress"/>
 
-    Cypress.on('uncaught:exception', (err, runnable) => {
-        // returning false here prevents Cypress from
-        // failing the test
-        return false
-    })
+const faker = require('faker')
+
+describe("Tests Login Page", () => {
 
     it("Test - Tryng to autenticate with correct User ID and password", () => {
-
-        const userId = 'abefortesting'
-        const firstName = 'Abe'
-        const lastName = 'For Testing'
-        const password = 'qwe123QWE!@#'
-
-        cy.get('#txtuserid').type(userId);
-        cy.get('#txtpassword').type(password);
-        cy.get('button[type=submit]').click();
+        cy.login()
 
         cy.get('h1')
-            .should("contain", `Welcome back, ${firstName}.`);
+            .should("contain", `Welcome back, ${Cypress.env('firstName')}.`);
     });
 
     it("Trying to authenticate with correct Email and password", () => {
-
-        //constants
-        const email = 'tixixir860@tlhao86.com'
-        const firstName = 'Abe'
-        const lastName = 'For Testing'
-        const password = 'qwe123QWE!@#'
-
-        //actions
-        cy.get('#txtuserid').type(email);
-        cy.get('#txtpassword').type(password);
-        cy.get('button[type=submit]').click();
+        cy.loginEmail()
 
         //validations
         cy.get('h1')
-            .should("contain", `Welcome back, ${firstName}.`);
+            .should("contain", `Welcome back, ${Cypress.env('firstName')}.`);
 
     });
 
     it("Trying to authenticate with incorrect User ID", () => {
+        const loginIncorrectUser = {
+            userId: `${faker.random.uuid()}`,
+            password: faker.random.words(1)
+        }
+        cy.userIncorrect(loginIncorrectUser)
+
+        cy.contains('Wrong user ID or password').should('exist')
 
     });
 
     it("Trying to authenticate with incorrect password", () => {
+        const loginIncorrectPassword = {
+            password: faker.random.words(1)
+        }
 
-    });
+        cy.passwordIncorrect(loginIncorrectPassword)
+
+        cy.contains('Wrong user ID or password').should('exist')
+    })
 
     it("Trying to authenticate by not entering any information in the User Id and password fields", () => {
+        cy.nullFields()
 
+        cy.get('.field-validation-error').should('not.be.disabled')
+        cy.contains('Invalid User ID').should('exist')
+        cy.contains('Wrong user ID or password').should('exist')
     });
 
 });
