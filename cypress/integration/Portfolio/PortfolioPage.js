438ad1ccd90510bb5c8ad0fbe5a24ffaa154e8d2
Pedro Faria <pedrofaria.mc@gmail.com>
2022-05-12 16:24:13 -0300

    Create portfolio

---
+++ cypress/integration/Portfolio/PortfolioPage.js
@@ -0,0 +1,26 @@
+/// <reference types="Cypress"/>
+
+const faker = require('faker')
+
+describe("Tests - Portfolio Page", () => {
+    it("Tests - Create a new Portfolio", () => {
+        const portfolioName = {
+            namePortfolio: faker.random.words(1),
+            portfolioDescription: faker.random.words(10)
+        }
+
+        cy.portfolioCreate(portfolioName);
+
+        cy.contains(portfolioName.namePortfolio)
+            .should('exist')
+    })
+
+//    it("Tests - Delete Portfolio", () => {
+        
+
+//        cy.portfolioDelete(portfolioName);
+
+//        cy.contains(portfolioName.namePortfolio)
+//            .should('exist')
+//    })
+//})
