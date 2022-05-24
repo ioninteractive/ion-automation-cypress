5ce882fc5ff8005483976826ece370472af62808
janaina.maciel <janaina.maciel@rockcontent.com>
2022-05-23 22:20:36 -0300

    New Scenarios

--- cypress/integration/Creative/CreativePage.js
+++ cypress/integration/Creative/CreativePage.js
@@ -13,11 +13,13 @@ describe("Tests - Creative Page", () => {
 
         cy.contains(creativeName.nameCreative)
             .should('exist')
+
+
     })
 })
 
 describe("Tests - Creative Page", () => {
-    it.only("Tests - Edit the creative created before", () => {
+    it("Tests - Edit the creative created before", () => {
         const editCreative = {
             creativeEdit: faker.random.words(2),
             creativeDescription: faker.random.words(10)
@@ -26,6 +28,34 @@ describe("Tests - Creative Page", () => {
         cy.editCreative(editCreative);
 
         cy.contains(editCreative.creativeEdit)
+            .should('exist')
+    })
+})
+
+describe("Tests - Creative Page", () => {
+    it("Tests - Copy a page of creative", () => {
+        const copyCreative = {
+            creativeCopy: faker.random.words(2),
+            creativeCopyDescription: faker.random.words(10)
+        }
+
+        cy.copyCreative(copyCreative);
+
+        cy.contains(copyCreative.creativeCopy)
+            .should('exist')
+    })
+})
+
+describe("Tests - Creative Page", () => {
+    it("Tests - Create a new creative from scratch", () => {
+        const startCreativeFromScratch = {
+            creativeFromScratch: faker.random.words(2),
+            creativeFromScratchDescription: faker.random.words(10)
+        }
+
+        cy.startCreativeFromScratch(startCreativeFromScratch);
+
+        cy.contains(startCreativeFromScratch.creativeFromScratchDescription)
             .should('exist')
     })
 })
