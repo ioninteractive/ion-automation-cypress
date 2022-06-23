620a051f84ee2fbfb796789ec822c42d26ae320e
Nicolas Kelepuris <nicolas.kelepuris@rockcontent.com>
2022-06-23 16:44:04 -0300

    create edit url test

--- cypress/support/gui_comands_Creative.js
+++ cypress/support/gui_comands_Creative.js
@@ -216,5 +216,95 @@ Cypress.Commands.add('createURL', createURL => {
 
 })
 
+Cypress.Commands.add('editURL', editURLInput => {
+    const { urlName, isRedirectType301, seoType, isRespondentsAlwaysNew, description, defaultURL, sitemapPriority, mediaTypeIndex, vehicleIndex, domainIndex } = editURLInput
+    cy.loginEmail()
+    const visitFirstUrlEditPage = () => {
+        cy.visit('Admin/Campaigns/Campaign/291#urls')
+        const firstUrlFullXPath = '/html/body/div[4]/div[3]/div[1]/div[3]/section[2]/div[2]/table/tbody/tr[1]/td[1]/a'
+        cy.xpath(firstUrlFullXPath).click({ force: true })
+        const editButtonFullXPath = '/html/body/div[4]/div[3]/div[1]/div[1]/div[2]/section[2]/div/a'
+        cy.wait(2000).then(_ => cy.xpath(editButtonFullXPath).click({ force: true }))
+        // cy.xpath(editButtonFullXPath).click({ force: true })
+    }
+
+    visitFirstUrlEditPage()
+
+    const editURL = () => {
+        if (isRedirectType301) {
+            cy.get('#inRedirect301Y').check({ force: true })
+        } else {
+            cy.get('#inRedirect301N').check({ force: true })
+        }
+    
+        switch (seoType) {
+            case 'Always':
+                cy.get('#inInSitemapA').check({ force: true })
+                break;
+            case 'Never':
+                cy.get('#inInSitemapN').check({ force: true })
+                break;
+            default:
+                cy.get('#inInSitemapP').check({ force: true })
+                break;
+        }
+    
+        if (isRespondentsAlwaysNew) {
+            cy.get('#inAlwaysNewY').check({ force: true })
+        } else {
+            cy.get('#inAlwaysNewN').check({ force: true })
+        }
+    
+        cy.get('#inSlashName').clear().type(urlName)
+        cy.get('#inDescription').clear().type(description)
+        cy.get('#inDefaultURL').clear().type(defaultURL)
+        cy.get('#inSitemapPriority').select(sitemapPriority)
+        cy.get('#inDomain').select(domainIndex).then(option => option.val()).as('expectedDomain')
+        cy.get('#inMediaType').select(mediaTypeIndex).then(option => option.val()).as('expectedMediaType')
+        cy.get('#inVehicle').select(vehicleIndex).then(option => option.val()).as('expectedVehicle')
+    }
+    editURL()
+
+    //save
+    cy.get('#btn_ts_save_edit').click({ force: true })
+
+    visitFirstUrlEditPage()
+    const assertURLWasEdited = () => {
+        if (isRedirectType301) {
+            cy.get('#inRedirect301Y').should('be.checked')
+        } else {
+            cy.get('#inRedirect301N').should('be.checked')
+        }
+    
+        switch (seoType) {
+            case 'Always':
+                cy.get('#inInSitemapA').should('be.checked')
+                break;
+            case 'Never':
+                cy.get('#inInSitemapN').should('be.checked')
+                break;
+            default:
+                cy.get('#inInSitemapP').should('be.checked')
+                break;
+        }
+    
+        if (isRespondentsAlwaysNew) {
+            cy.get('#inAlwaysNewY').should('be.checked')
+        } else {
+            cy.get('#inAlwaysNewN').should('be.checked')
+        }        
+        
+        cy.get('#inSlashName').should('have.value', urlName)
+        cy.get('#inDescription').should('have.value', description)
+        cy.get('#inDefaultURL').should('have.value', defaultURL)
+        cy.get('#inSitemapPriority option:selected').should('have.value', sitemapPriority)
+        cy.get('@expectedDomain').then(expectedDomain => cy.get('#inDomain').should('have.value', expectedDomain))
+        cy.get('@expectedMediaType').then(expectedMediaType => cy.get('#inMediaType').should('have.value', expectedMediaType))
+        cy.get('@expectedVehicle').then(expectedVehicle => cy.get('#inVehicle').should('have.value', expectedVehicle))
+    }
+
+    assertURLWasEdited()
+})
+
 
 
