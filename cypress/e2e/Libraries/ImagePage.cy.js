10b81ce5d1288cb26fe78f13ee16aab790a82a8e
Nicolas Kelepuris <nicolas.kelepuris@rockcontent.com>
2022-07-05 09:48:58 -0300

    refactor

---
+++ cypress/e2e/Libraries/ImagePage.cy.js
@@ -0,0 +1,25 @@
+describe("Tests - Realease", () => {
+    beforeEach(() => {
+        cy.loginEmail()
+    })
+    it("Tests - Insert Webp image in the library ", () => {
+        // const imageWebp = {
+        //     webpimage: `${faker.random.words(1)}AutomatedTest`,
+            
+        // }
+
+        // cy.imageWebp(imageWebp);
+        // cy.get('div[class="image-grid-column-name"]').should('have.text', 'testing.webp')
+
+    })
+    it("Tests - Use the webp image in the creative ", () => {
+        // const imageWebp = {
+        //     webpimage: `${faker.random.words(1)}AutomatedTest`,
+            
+        // }
+
+        // cy.imageWebp(imageWebp);
+        // cy.get('div[class="image-grid-column-name"]').should('have.text', 'testing.webp')
+
+    })
+})
