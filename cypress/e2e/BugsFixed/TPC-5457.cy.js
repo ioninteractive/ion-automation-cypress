a32fd122861c8c6b84e4422892f5d7cf02130848
Janaína Matheus <crisjanainamm@gmail.com>
2024-03-21 14:17:55 -0300

    Updating scripts on creativepage

---
+++ cypress/e2e/BugsFixed/TPC-5457.cy.js
@@ -0,0 +1,9 @@
+/*Preview button is redirecting to home page
+https://rock-content.atlassian.net/browse/TPC-5457
+
+Expected behavior: When clicking the preview button, it should open the preview of the Creative in a new tab.*/
+
+
+/// <reference types="Cypress"/>
+
+const faker = require('faker')
