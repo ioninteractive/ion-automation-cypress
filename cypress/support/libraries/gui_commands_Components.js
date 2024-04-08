13719ad21892d2422806bd970cde1ec161d031d7
Janaína Matheus <crisjanainamm@gmail.com>
2024-04-08 19:36:39 -0300

    Components Library

--- cypress/support/libraries/gui_commands_Components.js
+++ cypress/support/libraries/gui_commands_Components.js
@@ -14,7 +14,12 @@ Cypress.Commands.add('createNewLabel', input => {
     cy.get('input[class="select2-search__field"]').click()
     cy.get('input[placeholder="New Label"]').type(label)
     cy.get('button[data-action="addLabel"]').click()
-    cy.get('data-action="manageLabels"').click()
+    cy.get('button[data-action="manageLabels"]').click()
+    cy.contains(label).should('exist')
+
+    //another way to add a new label
+    cy.get('.search-field').type(label)
+    cy.get('.library-header__block-input > .btn').click()
     cy.contains(label).should('exist')
 
 
