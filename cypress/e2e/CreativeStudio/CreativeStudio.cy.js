3c80c0b2fc5f1c6d4be5edfe3f98b6c87a48b1a5
IONadmin1 <it@ioninteractive.com>
2026-08-31 00:18:23 -0400

    Migrate Creative Studio CDN assertions to Ion hostnames (task 7183)

--- cypress/e2e/CreativeStudio/CreativeStudio.cy.js
+++ cypress/e2e/CreativeStudio/CreativeStudio.cy.js
@@ -112,7 +112,7 @@ describe("Tests - Creative Studio - Optimized Images", () => {
         cy.visitLivePage({ creativeName })
         cy.url().as('livePageUrl')
         cy.get(`img[src*="${imageName}"]`).then($img => {
-            assert.isTrue($img.attr('src').startsWith('https://iuploads.scribblecdn.net/'), 'original images should be hosted by iuploads.scribblecdn.net')
+            assert.isTrue($img.attr('src').startsWith('https://cdn.ioninteractive.com/'), 'original images should be hosted by cdn.ioninteractive.com')
             expect($img[0].naturalWidth).to.be.greaterThan(0)
         })
         cy.get(`img[src*="${imageName}"]`).then($img => $img.attr('id')).as('imgId')
@@ -120,21 +120,21 @@ describe("Tests - Creative Studio - Optimized Images", () => {
         cy.setAlwaysOptimizeImage({ creativeName, imageName })
         cy.get('@livePageUrl').then(livePageUrl => cy.visit(livePageUrl))
         cy.get('@imgId').then(imgId => cy.get(`#${imgId}`).then($img => {
-            assert.isTrue($img.attr('src').startsWith('https://ion-imagesizer.scribblecdn.net/'), 'optimized images should be hosted by ion-imagesizer.scribblecdn.net')
+            assert.isTrue($img.attr('src').startsWith('https://imagesizer.ioninteractive.com/'), 'optimized images should be hosted by imagesizer.ioninteractive.com')
             expect($img[0].naturalWidth).to.be.greaterThan(0)
         }))
         
         cy.setInheritOptimizeImageBehaviorFromLibrary({ creativeName, imageName })
         cy.get('@livePageUrl').then(livePageUrl => cy.visit(livePageUrl))
         cy.get('@imgId').then(imgId => cy.get(`#${imgId}`).then($img => {
-            assert.isTrue($img.attr('src').startsWith('https://iuploads.scribblecdn.net/'), 'original images should be hosted by iuploads.scribblecdn.net')
+            assert.isTrue($img.attr('src').startsWith('https://cdn.ioninteractive.com/'), 'original images should be hosted by cdn.ioninteractive.com')
             expect($img[0].naturalWidth).to.be.greaterThan(0)
         }))
         
         cy.optimizeImage({ category: imageCategory, name: imageName })
         cy.get('@livePageUrl').then(livePageUrl => cy.visit(livePageUrl))
         cy.get('@imgId').then(imgId => cy.get(`#${imgId}`).then($img => {
-            assert.isTrue($img.attr('src').startsWith('https://ion-imagesizer.scribblecdn.net/'), 'optimized images should be hosted by ion-imagesizer.scribblecdn.net')
+            assert.isTrue($img.attr('src').startsWith('https://imagesizer.ioninteractive.com/'), 'optimized images should be hosted by imagesizer.ioninteractive.com')
             expect($img[0].naturalWidth).to.be.greaterThan(0)
         }))
     })
